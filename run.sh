7b5aa8686b4d059bf313513ce8ceea1ccec31227
Byan Xia <616613288@qq.com>
2023-11-26 16:05:55 +0800

    Normal Version

---
+++ run.sh
@@ -0,0 +1,11 @@
+for i in 10
+do
+      echo "runing Pavia"
+      python demo.py --dataset="Pavia" --train_num=$i --epoches=480 --train_time=4 --patches=7 --band_patches=3
+done
+
+for i in 10
+do
+      echo "runing Honghu"
+      python demo.py --dataset="Honghu" --train_num=$i --epoches=300 --train_time=4 --patches=3 --band_patches=7
+done
